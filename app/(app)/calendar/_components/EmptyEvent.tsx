eb2a13f34a98c3c648add05c7f0c5934254f8737
강동욱 <ehddnr8563@gmail.com>
2025-12-20 14:48:37 +0900

    chore: EmptyEvent 디자인 수정

--- app/(app)/calendar/_components/EmptyEvent.tsx
+++ app/(app)/calendar/_components/EmptyEvent.tsx
@@ -2,11 +2,11 @@ import { CalendarDaysIcon } from 'lucide-react';
 
 export default function EmptyEvent() {
   return (
-    <div className='border-2 border-dashed p-3 rounded-xl bg-slate-50'>
+    <div className='border-2 border-dashed border-border p-16 text-center rounded-3xl bg-slate-50'>
       <div className='flex flex-col justify-center items-center'>
-        <CalendarDaysIcon className='w-8 h-8 flex items-center justify-center text-slate-600' />
+        <CalendarDaysIcon className='w-8 h-8 flex items-center justify-center text-slate-500' />
         <p className='text-base font-semibold text-slate-500 mt-4'>
-          일정이 없습니다.
+          등록된 일정이 없습니다.
         </p>
       </div>
     </div>
